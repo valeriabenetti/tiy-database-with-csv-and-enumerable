10f60c929c15e759b29dd9d18f0156dcda13a74f
Valeria Benetti <vbenetti46@gmail.com>
2017-03-25 15:11:53 -0400

    wrtie to csv and delete from works

--- tiydatabase.rb
+++ tiydatabase.rb
@@ -1,17 +1,16 @@
 require 'csv'
 class Person
   # Saving the correct data into class
-  attr_reader 'name', 'phone_number', 'address', 'position', 'salary', 'slack_account', 'github_account'
-
-  # Defining name, phone_number, address, position, salary,slack_account, github_account
-  def initialize(name, phone_number, address, position, salary, slack_account, github_account)
+  attr_reader 'name', 'phone', 'address', 'position', 'salary', 'slack', 'github'
+  # Defining name, phone, address, position, salary,slack, github
+  def initialize(name, phone, address, position, salary, slack, github)
     @name = name
-    @phone_number = phone_number
+    @phone = phone
     @address = address
     @position = position
     @salary = salary
-    @slack_account = slack_account
-    @github_account = github_account
+    @slack = slack
+    @github = github
   end
 end
 
@@ -22,12 +21,16 @@ class Tiydatabase
     @accounts = []
     CSV.foreach("employees.csv", headers: true) do |row|
       name = row["name"]
-      phone_number = row["phone_number"]
+      phone = row["phone"]
       address = row["address"]
       position = row["position"]
       salary = row["salary"]
-      slack_account = row["slack_account"]
-      github_account = row["github_account"]
+      slack = row["slack"]
+      github = row["github"]
+
+      account = Person.new(name, address, phone, position, salary.to_i, slack, github)
+
+      @accounts << account
     end
   end
 
@@ -38,7 +41,7 @@ class Tiydatabase
       puts "Said human exists in the database!"
     else
       print 'What is their phone number?'
-      phone_number = gets.chomp.to_i
+      phone = gets.chomp.to_i
 
       print 'What is their address?'
       address = gets.chomp
@@ -50,30 +53,32 @@ class Tiydatabase
       salary = gets.chomp.to_i
 
       print 'What is their Slack username?'
-      slack_account = gets.chomp
+      slack = gets.chomp
 
       print 'What is their GitHub username?'
-      github_account = gets.chomp
+      github = gets.chomp
 
-      account = Person.new(name, phone_number, address, position, salary, slack_account, github_account)
+      account = Person.new(name, phone, address, position, salary, slack, github)
 
       @accounts << account
+
     end
+    write_csv
   end
 
   def search_person
     puts 'Please input the name of the person or account information that you are searching for? '
     search_person = gets.chomp
-    found_account = @accounts.find { |account| account.name.include?(search_person) || account.slack_account == search_person || account.github_account == search_person}
+    found_account = @accounts.find { |account| account.name.include?(search_person) || account.slack == search_person || account.github == search_person}
     if found_account
       puts "This is #{found_account.name}'s information.
        \nName: #{found_account.name}
-       \nPhone: #{found_account.phone_number}
+       \nPhone: #{found_account.phone}
        \nAddress: #{found_account.address}
        \nPosition: #{found_account.position}
        \nSalary: #{found_account.salary}
-       \nSlack Account: #{found_account.slack_account}
-       \nGitHub Account: #{found_account.github_account}"
+       \nSlack Account: #{found_account.slack}
+       \nGitHub Account: #{found_account.github}"
     else
       puts "#{search_person} is not in our system.\n"
     end
@@ -83,25 +88,26 @@ class Tiydatabase
     puts 'Who are you looking to terminate? '
     name_deleted = gets.chomp
 
-    delete_account = accounts.find { |account| account.name == name_deleted }
+    delete_account = @accounts.delete_if { |account| account.name == name_deleted }
     if delete_account
       puts 'Account has been exterminated!'
-      accounts.delete(delete_account)
+      write_csv
     else
       puts 'No such account exist'
     end
+
   end
 
   def report_account
     sorted_accounts = @accounts.sort_by {|account| account.name }
     sorted_accounts.each do |account|
-      puts "The Iron Yard Database Reports: "
-      puts "The total salary for the Instructors is #{teacher_salary}"
-      puts "The total salary for the Campus Director is #{director_salary}"
-      puts "The total number of students at the Iron Yard is #{total_students}"
-      puts "The total number of Instructor at the Iron Yard is#{total_teachers}"
-      puts "The total number of Campus Directors at the Iron Yard is #{total_director}"
     end
+    puts "The Iron Yard Database Reports: "
+    puts "The total salary for the Instructors is #{teacher_salary}"
+    puts "The total salary for the Campus Director is #{director_salary}"
+    puts "The total number of students at the Iron Yard is #{total_students}"
+    puts "The total number of Instructor at the Iron Yard is #{total_teachers}"
+    puts "The total number of Campus Directors at the Iron Yard is #{total_director}"
 
   end
 
@@ -127,9 +133,9 @@ class Tiydatabase
 
   def write_csv
     CSV.open("employees.csv", "w") do |csv|
-      csv << ["name", "phone_number", "address", "position", "salary", "slack_account", "github_account"]
+      csv << ["name", "phone", "address", "position", "salary", "slack", "github"]
       @accounts.each do |account|
-        csv << [account.name, account.phone_number, account.address, account.position, account.salary, account.slack_account, account,github_account]
+        csv << [account.name, account.phone, account.address, account.position, account.salary, account.slack, account.github]
       end
     end
   end
@@ -148,3 +154,6 @@ loop do
 
   data.report_account if selected == 'R'
 end
+# Gavin,555-1212,1 Main Street,Instructor,1000000,gstark,gstark
+# Jason,555-4242,500 Elm Street,Instructor,2000000,ambethia,ambethia
+# Toni,555-4444,200 Pine Street,Campus Director,3000000,amazing_toni,amazing_toni
